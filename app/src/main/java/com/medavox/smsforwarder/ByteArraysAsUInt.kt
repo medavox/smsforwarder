6b5b41272dfc48c90a915a8ce0ac4d3db15f02a8
Adam Howard <achdisposable1+github@gmail.com>
2018-05-24 10:27:34 +0100

    improve ByteArraysAsUInt.kt
    write a faster implementation of plus which doesn't rely on inc() and dec()
    write conversion functions to and from int and long
    fix small bugs discovered during testing

--- app/src/main/java/com/medavox/smsforwarder/ByteArraysAsUInt.kt
+++ app/src/main/java/com/medavox/smsforwarder/ByteArraysAsUInt.kt
@@ -1,6 +1,10 @@
 package com.medavox.smsforwarder
 
+import com.medavox.util.io.Bytes
 import unsigned.toUbyte
+import java.lang.Math.abs
+import java.lang.Math.pow
+import kotlin.math.pow
 
 /**
  * @author Adam Howard
@@ -20,7 +24,7 @@ Allows you to perform basic arithmetic operations on arbitrary-length
  */
 
 /**Decrement the passed byte[]-uint. NOTE:
- * this method does not underflow; a call to decrement on an array odf all 0s
+ * this method does not underflow; a call to decrement on an array of all 0s
  * merely returns the original array*/
 operator fun ByteArray.dec(): ByteArray  {
     if(this.equalsZero()) {
@@ -30,13 +34,18 @@ operator fun ByteArray.dec(): ByteArray  {
     val ret = this.copyOf()
     for(i in 0 until ret.size) {
         val ub = ret[i].toUbyte()
+
         if(ub > 0) {
+            //System.out.println("ubyte $i \"$ub\" is nonzero")
             ret[i] = (ub-1).toByte()
             return ret
         }
         else {//byte is 0; we need to set this byte to 255,
             // then move to the next byte to decrement there
+            //System.out.println("byte $i is zero; new value: ${255.toUbyte()}")
+            //System.out.println("ubyte \"$ub\" ($i of ${ret.size}) is not > 0")
             ret[i] = 255.toUbyte().toByte()
+            //System.out.println("new value: ${ret[i].toUbyte()}; > 0: ${ret[i].toUbyte() > 0}")
             //because of the above guard clause,
             //we know that at least one byte in this array is non-zero
             //so we can just keep setting 0 bytes to 255
@@ -59,7 +68,7 @@ operator fun ByteArray.inc(): ByteArray {
         }
         else {//byte is 255
             ret[i] = 0
-            if(i < ret.size-1) {//array is too small to fit a new byte
+            if(i >= ret.size-1) {//array is too small to fit a new byte
                 ret = ret.copyOf(ret.size+1)//grow the array
                 ret[i+1] = 1//the new byte is 0, so no need to check: carry the 1!
                 return ret
@@ -74,7 +83,7 @@ operator fun ByteArray.inc(): ByteArray {
 
 /**Adds a and b, and returns an array of the same size of a.
  * The returned array grows large enough to contain the result.*/
-operator fun ByteArray.plus(b2: ByteArray): ByteArray {
+fun ByteArray.oldplus(b2: ByteArray): ByteArray {
     var toFill:ByteArray
     var toEmpty:ByteArray
     //move 1s from smaller array into larger array
@@ -92,14 +101,53 @@ operator fun ByteArray.plus(b2: ByteArray): ByteArray {
     return toFill
 }
 
+
+/**Adds a and b, and returns an array of the same size of a.
+ * The returned array grows large enough to contain the result.
+ * this implementation is faster than the old one.*/
+operator fun ByteArray.plus(b2: ByteArray): ByteArray {
+    var toFill:ByteArray
+    val toEmpty:ByteArray
+    //move 1s from smaller array into larger array
+    if(this.size > b2.size) {
+        toFill = this.copyOf()
+        toEmpty = b2.copyOf()
+    }else{
+        toFill = b2.copyOf()
+        toEmpty = this.copyOf()
+    }
+    var carryLast = false
+    for (i in toEmpty.indices) {
+        val carry = if(carryLast) 1 else 0
+        val sum = toFill[i] + toEmpty[i] + carry
+        if(sum > 255) {
+            carryLast = true
+            toFill[i] = (sum % 256).toByte()
+        }else {
+            carryLast = false
+            toFill[i] = sum.toByte()
+        }
+    }
+    if(carryLast) {//if the last element had a carry, create a new one
+        toFill = toFill.copyOf(toFill.size+1)
+        toFill[toFill.size-1] = 1
+    }
+    return toFill
+}
+
 /**Subtracts b from a, and returns an array the size of a.
  * if b > a, returns 0.*/
-operator fun ByteArray.minus(b: ByteArray): ByteArray {
+operator fun ByteArray.minus(byteArray: ByteArray): ByteArray {
     var a = this.copyOf()
-    var b2 = b.copyOf()
-    while (!b2.equalsZero()) {
-        a--
-        b2--
+    var b = byteArray.copyOf()
+    while (!b.equalsZero() && !a.equalsZero()) {
+        --a
+        --b
+        //System.err.println("a: ${Bytes.bytesToHex(a)}; b2: ${Bytes.bytesToHex(b)}")
+        /*if(b.equalsZero()) {
+            System.out.println("b equals zero; a:${Bytes.bytesToHex(a)}")
+            break
+        }*/
     }
     return a
 }
@@ -129,7 +177,7 @@ operator fun ByteArray.div(b: ByteArray): ByteArray {
 /**return the result of a % b */
 operator fun ByteArray.rem(b: ByteArray): ByteArray {
     var a = this.copyOf()
-    while (a > b) {
+    while (a >= b) {
         a -= b
     }
     return a
@@ -139,21 +187,21 @@ operator fun ByteArray.compareTo(b: ByteArray): Int {
     if(this.size > b.size) {//this is larger; checks its higher elements for any nonzero bytes
         for (i in this.size-1 downTo b.size) {
             if (this[i] != 0.toByte()) {
-                return 1;
+                return 1
             }
         }
     }
     if(b.size > this.size) {//b is larger, do the same for b
         for (i in b.size-1 downTo this.size) {
             if (b[i] != 0.toByte()) {
-                return -1;
+                return -1
             }
         }
     }
     //else the arrays are of equal size, or the larger array contains only 0s in its higher bytes
     for (i in this.size downTo 0) {
-        if(this[0] != b[0]) {
-            return this[0].compareTo(b[0])
+        if(this[i] != b[i]) {
+            return this[i].compareTo(b[i])
         }
     }
     //everything is equal
@@ -176,4 +224,67 @@ fun newZeroedBytes(length: Int=1): ByteArray {
         result[i] = 0x00.toByte()
     }
     return result
+}
+
+
+@Throws(NumberFormatException::class)
+fun ByteArray.toInt(): Int {
+    var out:Int = 0
+    for(i in this.indices) {
+        val multiplier = 256.pow(i)
+        val addition = this[i] * multiplier
+        if((addition.toLong() + out.toLong()) > Int.MAX_VALUE ) {
+            throw NumberFormatException("value of ByteArray is too high to store in an int")
+        }
+        out += addition
+    }
+    return out
+}
+
+@Throws(NumberFormatException::class)
+fun ByteArray.toIntWithOverflow(): Int {
+    var out:Int = 0
+    for(i in this.indices) {
+        val multiplier = 256.pow(i)
+        out += this[i] * multiplier
+    }
+    return out
+}
+
+@Throws(NumberFormatException::class)
+fun ByteArray.toLong(): Long {
+    var out: Long = 0
+    var a = this.copyOf()
+    while (!a.equalsZero()) {
+        if (out == java.lang.Long.MAX_VALUE) {
+            throw NumberFormatException("value of argument byte[] is too high to store in a long")
+        }
+        a--
+        out++
+    }
+    return out
+}
+
+@Throws(NumberFormatException::class)
+fun fromLong(input: Long): ByteArray {
+    var l = input
+    if (l < 0) {
+        throw NumberFormatException("Argument cannot be < 0. This data type is unsigned!")
+    }
+    var out = newZeroedBytes(8)
+    while (l > 0) {
+        out++
+        l--
+    }
+    return out
+}
+
+private fun Int.pow(exp:Int): Int {
+    if(exp == 0) {
+        return this
+    }
+    val times = abs(exp)
+    var subj = this
+    repeat(times, {subj *= this})
+    return if(exp < 0) -subj else subj
 }
